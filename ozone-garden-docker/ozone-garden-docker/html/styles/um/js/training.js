638777e559fee1697a30a9ba795d920689d84676
user <tale9912@colorado.edu>
2020-03-18 16:57:14 -0600

    leaf picture randomization

---
+++ ozone-garden-docker/ozone-garden-docker/html/styles/um/js/training.js
@@ -0,0 +1,47 @@
+'use strict';
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
+function getFilePath() {
+    var filepath = "/Images/ozone/1.jpg";
+    const type = getRandomInt(3); 
+    if(type == 0) { // no injury
+        filepath = "/Images/".concat("noinjury/", getRandomInt(2), ".jpg")
+    }
+    if(type == 1) { // ozone
+        filepath = "/Images/".concat("ozone/", getRandomInt(3), ".jpg")
+    }
+    if(type == 2) { // other dmg
+        filepath = "/Images/".concat("other/", getRandomInt(3), ".jpg")
+    }  
+
+    return filepath;
+}
+
+const e = React.createElement;
+
+class NextQuestion extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = { 
+            file: "/Images/ozone/1.jpg",
+        };
+    }
+
+    render() {
+        document.getElementById("displayed_leaf").src = this.state.file;
+
+        return e(
+            "button",
+            { onClick: () => this.setState({ file: getFilePath() }) },
+            "Next Question"
+        );
+    }
+}
+
+const domContainer = document.getElementById("next_question");
+ReactDOM.render(e(NextQuestion), domContainer);
